726c6af1ed54e491a527e7fce9b08a3a9c476f54
sebastian <104945395+jsebastian2707@users.noreply.github.com>
2025-04-11 01:08:01 -0500

    renombrar ruta usuario

--- back/index.js
+++ back/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 // 🚓 routes
-const usuariosRoutes = require ("./routes/usuarios.routes.js");
+const usuarioRoutes = require ("./routes/usuario.routes.js");
 const servicioRoutes = require ("./routes/servicio.routes.js");
 
 const app = express();
 app.use(express.json());
-app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/servicios', servicioRoutes);
 
 const PORT = 3000;
