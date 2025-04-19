cab158ee5b0a7639a45b599f082a8b71507ff949
sebastian <104945395+jsebastian2707@users.noreply.github.com>
2025-04-19 00:06:24 -0500

    fix cors in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const cors = require('cors');
 // 🚓 routes
 const usuarioRoutes = require ("./routes/usuario.routes.js");
 const servicioRoutes = require ("./routes/servicio.routes.js");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/servicios', servicioRoutes);
